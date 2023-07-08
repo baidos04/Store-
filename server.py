18c1add246382694d04ef1ccd273c5a7c96361a0
baidos04 <muslimovbaidoskii@gmail.com>
2023-07-08 20:10:25 +0600

    query response received

--- server.py
+++ server.py
@@ -1,55 +1,44 @@
 import requests
-import json
 
-API_URL = "https://test-docs.stores.kg/api"
-API_TOKEN = "api-token: 00e82b9c923ba5851821bc8215004d26"
-
-SOFTECH = [
-    {"name": "Xiaomi Redmi Note 10", "id": 1},
-    {"name": "Samsung Galaxy S21", "id": 2},
-    {"name": "iPhone 12 Pro", "id": 3},
-]
+API_URL = "https://test-docs.stores.kg/api/products"
+API_TOKEN = "00e82b9c923ba5851821bc8215004d26"
 
 def make_api_request():
     headers = {
         "api-token": API_TOKEN,
     }
 
-    for phone in SOFTECH:
-        params = {
-            "page": 1,
-            "itemsPerPage": 30,
-            "order[price]": "asc",
-            "name": phone["name"],
-            "point.id": phone["id"]
-        }
+    params = {
+        "page": 1,
+        "itemsPerPage": 30,
+        "order[price]": "asc",
+        "order[name]": "asc"
+    }
 
-        try:
-            response = requests.get(API_URL, headers=headers, params=params)
-            response.raise_for_status()
+    try:
+        response = requests.get(API_URL, headers=headers, params=params)
+        response.raise_for_status()
 
-            data = response.json()
+        data = response.json()
 
-            if "hydra:member" in data:
-                products = data["hydra:member"]
-                sorted_products = sort_products_by_name(products)
-                process_products(sorted_products)
-            else:
-                print("Отсутствуют продукты в ответе API.")
+        if "hydra:member" in data:
+            products = data["hydra:member"]
+            process_products(products)
+        else:
+            print("Отсутствуют продукты в ответе API.")
 
-        except requests.exceptions.RequestException as e:
-            print("Ошибка при выполнении запроса:", str(e))
-
-def sort_products_by_name(products):
-    sorted_products = sorted(products, key=lambda p: p["name"])
-    return sorted_products
+    except requests.exceptions.RequestException as e:
+        print("Ошибка при выполнении запроса:", str(e))
 
 def process_products(products):
-    json_data = json.dumps(products, indent=4)
-    print(json_data)
-
+    for product in products:
+        product_id = product["id"]
+        product_name = product["name"]
+        print(f"Product ID: {product_id}, Name: {product_name}")
 
 make_api_request()
+
+
 
 
 
